798da826eff6a4453907ace677d6e5f9be08f9d1
guanyi <guanyifang@hotmail.com>
2015-01-25 09:00:24 -0800

    almost done, only sql injection check left.

--- lab2.php
+++ lab2.php
@@ -1,7 +1,9 @@
 <?php
 
-$sub = $_POST['subject'];
-$input = $_POST['text'];
+define("PRIVATELINE", PHP_EOL . "private private private private private!" . PHP_EOL);
+
+$sub = $_GET['subject'];
+$input = $_GET['text'];
 
 $file = fopen("data.txt", "w+") or die ("error");
 
@@ -10,12 +12,22 @@ $input = trim($input);
 if($sub == "" or $input == "" ) {
     die("Missing required data.");
 }
-$sub = strip_tags($sub);
-$input = strip_tags($input);
 
-$sub = mysql_real_escape_string($sub);
-$input = mysql_real_escape_string($sub);
-echo $sub;
-echo $input;
-fwrite($file, $sub . PHP_EOL);
+if($sub != strip_tags($sub) or $input != strip_tags($input)) {
+    die("breakin attempt detected");
+}
+
+//$sub = mysql_real_escape_string($sub);
+//$input = mysql_real_escape_string($sub);
+
+if (isset($_GET['private'])) {
+    fwrite($file, PRIVATELINE);
+}
+fwrite($file, PHP_EOL . "\n-----\n" . PHP_EOL);
+fwrite($file, PHP_EOL . $sub . PHP_EOL);
+if (filter_var($sub, FILTER_VALIDATE_EMAIL)) {
+    fwrite($file, "(email address)" . PHP_EOL);
+}
+fwrite($file, PHP_EOL . $input . PHP_EOL);
+fwrite($file, 'POSTED AT ' . date('g:ia') . ' ON ' . strtoupper(date('l F d, Y')));
 fclose($file);
